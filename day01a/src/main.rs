8596fb0971c7ed9cde47df4021e7208067084d4c
Jack Darlison <jdarlison7@gmail.com>
2023-12-01 15:42:19 +0000

    day01a

---
+++ day01a/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("{}",
+        include_str!("../input.txt")
+            .split("\n")
+            .fold(0, |acc: i32, s: &str| {
+                let left: char = s.chars().find(|c| c.is_numeric()).unwrap();
+                let right: char = s.chars().rfind(|c| c.is_numeric()).unwrap();
+                acc + String::from_iter([left, right]).parse::<i32>().unwrap()
+            })
+    )
+}
