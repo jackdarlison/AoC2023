124b9db16cfc398ff9d037597be782f65ae881ab
Jack Darlison <jdarlison7@gmail.com>
2023-12-03 09:57:33 +0000

    removed unused vars

--- day02b/src/main.rs
+++ day02b/src/main.rs
@@ -1,13 +1,11 @@
 use regex::Regex;
 
 pub fn main() {
-    let game_re = Regex::new(r"Game (?<game>[0-9]+): ").unwrap();
     let colour_re = Regex::new(r"(?<num>[0-9]+) (?<col>(?:green|red|blue))").unwrap();
     println!("{}",
         include_str!("../input.txt")
             .split("\n")
             .fold(0, |acc: usize, s: &str| {
-                let game_caps = game_re.captures(s).unwrap();
                 let (r, g, b) = colour_re.captures_iter(s).fold((0, 0, 0), |(r, g, b), c| {
                     let num: usize = c["num"].parse::<usize>().unwrap();
                     match &c["col"] {
